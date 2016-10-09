7a53ff02eb8af8cb61763e62eaeaee982e6be992
Victoria Taylor <victoria.m.taylor.17@dartmouth.edu>
2016-10-09 18:53:50 -0400

    comments

--- challenge.js
+++ challenge.js
@@ -5,32 +5,34 @@ window.onload = function(){
 	var lightbox = document.getElementById("lightbox");
 	var script = document.createElement('script');
 	script.type = 'text/javascript';
-	var galleryID = "72157663613668085";
-	var bo = "72157665057503822";
+	var galleryID = "72157665057503822";
 	script.async = true; 
-	script.src = flickrURL + "method=flickr.galleries.getPhotos&api_key=" + API_KEY + "&gallery_id=" + bo + "&jsoncallback=pictureshandler&format=json";
+	script.src = flickrURL + "method=flickr.galleries.getPhotos&api_key=" + API_KEY + "&gallery_id=" + galleryID + "&jsoncallback=addPictures&format=json";
 	document.getElementsByTagName('head')[0].appendChild(script);
 }
 
-//refactor
-function pictureshandler(data){
+//callback for flickr API call
+function addPictures(data){
 	var numPhotos = Math.min(data.photos.photo.length, 50);
 	var photos = data.photos.photo;
 	var lightbox = document.getElementById("lightbox");
 	var photoArray = [];
 
+	//Add thumbnail for each image
 	for (var i = 0; i < numPhotos; i++){
 		var newImage = document.createElement('img');
 		var imgSrc = "https://farm" + photos[i].farm + ".staticflickr.com/" + photos[i].server + "/" + photos[i].id + "_" + photos[i].secret;
-		newImage.src = imgSrc + "_q.jpg"; //small photo
+		newImage.src = imgSrc + "_q.jpg"; //small square photo
 		var container = document.createElement('div');
 		container.className = 'thumbnail';
 		container.setAttribute('imageNum', i);
 		container.appendChild(newImage);
 		document.getElementById('image-container').appendChild(container);
 	
-		photoArray[i] = {'title': photos[i].title, 'src': imgSrc + "_z.jpg"};
+		//for clicking through images in lightbox view
+		photoArray[i] = {'title': photos[i].title, 'src': imgSrc + "_z.jpg"};	//larger img src
 
+		//opens lightbox view
 		container.addEventListener('click', function(event){
 			lightbox.style.display = "block";
 			var index = event.target.parentElement.getAttribute('imageNum');
@@ -40,7 +42,8 @@ function pictureshandler(data){
 		})
 	}
 
-
+	//Click handling when lightbox is open
+	//click outside of images closes lightbox, on image goes to next image, arrows navigate through images
 	lightbox.addEventListener('click', function(event){
 		var nextIndex, img; 
 
